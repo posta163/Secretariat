a571ba411656567e3cf6e0a735887345a85ffeaf
Tomasz Rojek <tiger243526@gmail.com>
2026-08-31 20:14:57 +0200

    add correspondence list endpoint

---
+++ Secretariat.Api/Controllers/CorrespondenceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Secretariat.Api.Data;
+using Secretariat.Api.Models;
+
+namespace Secretariat.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CorrespondenceController : ControllerBase
+    {
+        private readonly SecretariatDbContext _context;
+
+        public CorrespondenceController(SecretariatDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Correspondence>>> GetAll()
+        {
+            var correspondences = await _context.Correspondences.ToListAsync();
+
+            return Ok(correspondences);
+        }
+    }
+}
